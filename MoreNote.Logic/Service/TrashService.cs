a9708c3842e8b619390d47a16e1ae3de8493091b
hyfree <wangxianqiangcn@foxmail.com>
2020-01-27 11:57:52 +0800

    trash需要实现的API

--- MoreNote.Logic/Service/TrashService.cs
+++ MoreNote.Logic/Service/TrashService.cs
@@ -1,10 +1,57 @@
-﻿using System;
+﻿using MoreNote.Logic.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MoreNote.Logic.Service
 {
-    class TrashService
+    public class TrashService
     {
+        // 回收站
+        // 可以移到noteSerice中
+
+        // 不能删除notebook, 如果其下有notes!
+        // 这样回收站里只有note
+
+        // 删除笔记后(或删除笔记本)后入回收站
+        // 把note, notebook设个标记即可!
+        // 已经在trash里的notebook, note不能是共享!, 所以要删除共享
+
+
+        // 删除note
+        // 应该放在回收站里
+        // 有trashService
+        public static bool DeleteNote(long noteId,long userId)
+        {
+            throw new Exception();
+        }
+        // 删除别人共享给我的笔记
+        // 先判断我是否有权限, 笔记是否是我创建的
+        public static bool DeleteSharedNote(long noteId,long myUserId)
+        {
+            throw new Exception();
+        }
+        // recover
+        public static bool  recoverNote(long noteId,long notebookId,long userId)
+        {
+            throw new Exception();
+        }
+        // 删除trash
+        public static bool DeleteTrash(long noteId,long userId)
+        {
+            throw new Exception();
+        }
+        public static bool DeleteTrashApi(long noteId,long userId,int usn)
+        {
+            throw new Exception();
+        }
+        // 列出note, 排序规则, 还有分页
+        // CreatedTime, UpdatedTime, title 来排序
+        public static Note[] ListNotes(long userId,int pageNumber,int pageSize,string sortField,bool isAsc)
+        {
+            throw new Exception();
+        }
+
+
     }
 }
