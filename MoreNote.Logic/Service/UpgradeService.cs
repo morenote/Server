54b3f5c92bb59173b17d9d3559a2afef3f79a032
hyfree <wangxianqiangcn@foxmail.com>
2020-01-27 12:01:35 +0800

    UpgradeService需要实现的方法

--- MoreNote.Logic/Service/UpgradeService.cs
+++ MoreNote.Logic/Service/UpgradeService.cs
@@ -4,8 +4,45 @@ using System.Text;
 
 namespace MoreNote.Logic.Service
 {
-    class UpgradeService
+    public class UpgradeService
     {
+        // 添加了PublicTime, RecommendTime
+        public static bool UpgradeBlog()
+        {
+            throw new Exception();
+        }
+        // 11-5自定义博客升级, 将aboutMe移至pages
+        /*
+        <li>Migrate "About me" to single(a single post)</li>
+        <li>Add some default themes to administrator</li>
+        <li>Generate "UrlTitle" for all notes. "UrlTitle" is a friendly url for post</li>
+        <li>Generate "UrlTitle" for all notebooks</li>
+        <li>Generate "UrlTitle" for all singles</li>
+        */
+        public static bool UpgradeBetaToBeta2(long userId)
+        {
+            throw new Exception();
+        }
+        // Usn设置
+        // 客户端 api
+        public static void moveTag()
+        {
+            throw new Exception();
+        }
+        public static void setNotebookUsn()
+        {
+            throw new Exception();
+        }
+        public static void setNoteUsn()
+        {
+            throw new Exception();
+        }
+        // 升级为Api, beta.4
+        public static bool Api(long userId)
+        {
+            throw new Exception();
+
+        }
 
     }
 }
