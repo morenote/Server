b795d993595520ea91ce61b21387de4bf7c64ebc
hyfree <wangxianqiangcn@foxmail.com>
2020-02-07 09:37:28 +0800

    增加关于图片的todo🔨

--- MoreNote.Logic/Entity/NoteFileInfo.cs
+++ MoreNote.Logic/Entity/NoteFileInfo.cs
@@ -17,6 +17,11 @@ namespace MoreNote.Logic.Entity
         public Int64 Size { get; set; } // file  size (byte)
         public string Type { get; set; } // file  type ""=image "doc"=word
         public string Path { get; set; } // the file path
+        //0 public 1 protected 2 private
+        //公开 所有人可以访问
+        //保护 任何允许访问笔记的人可以允许访问
+        //私有 仅允许笔记拥有者访问
+        public int AccessPermission { get;set; }
 
         public bool IsDefaultAlbum { get; set; }
         public DateTime CreatedTime { get; set; }
