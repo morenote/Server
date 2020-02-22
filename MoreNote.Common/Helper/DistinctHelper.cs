26bd91ceb05523b9ab4b3d204e5af9baa3f31d76
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-02-23 00:27:17 +0800

    增加分类功能📈

---
+++ MoreNote.Common/Helper/DistinctHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoreNote.Common.Helper
+{
+    public static class DistinctHelper
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var identifiedKeys = new HashSet<TKey>();
+            return source.Where(element => identifiedKeys.Add(keySelector(element)));
+        }
+    }
+}
