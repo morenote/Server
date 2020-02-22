43964e860f5d765823c3106bbcdb3fe0ea7775f8
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-02-22 21:03:29 +0800

    更新框架📕

---
+++ 通用类库测试/Utils/HtmlToTextHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MoreNote.Common.Utils;
+
+namespace MoreNote.Common.Utils.Tests
+{
+    [TestClass()]
+    public class HtmlToTextHelperTests
+    {
+        [TestMethod()]
+        public void ConvertTest()
+        {
+            HtmlToTextHelper convert = new HtmlToTextHelper();
+            string md_text = File.ReadAllText(@"C:\Users\WangXianQiang\Desktop\content.md");
+            string html = "";
+            using (var reader = new StringReader(md_text))
+            {
+                using (var writer = new StringWriter())
+                {
+                    CommonMark.CommonMarkConverter.Convert(reader, writer);
+                    //writer.ToString()即为转换好的html
+                    html = writer.ToString();
+                }
+            }
+            string text = convert.Convert(html);
+            Console.WriteLine(text);
+
+
+        }
+    }
+}
