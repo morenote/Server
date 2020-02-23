500c16103146b6f30f5106acc2e0f25fab212e43
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-02-23 08:43:55 +0800

    增加伪JWT校验机制🔓

---
+++ MoreNote.Logic/Model/JWT.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MoreNote.Common.Util;
+using MoreNote.Common.Utils;
+
+namespace MoreNote.Logic.Model
+{
+    public class JWT
+    {
+        public JWT_Header Header { get; set; }
+        public JWT_Payload Payload { get; set; }
+
+        public string Signature { get; set; }
+
+        public static JWT GetJWTFormBase64(string base64)
+        {
+            return null;
+        }
+        public static JWT GetJWTFormJson(string base64)
+        {
+            return null;
+        }
+        public static JST GetJWT(long tokenId,string userNmae,long userId, string group, long exp= 31536000)
+        {
+
+            JWT_Header header = new JWT_Header()
+            {
+                alg = "sha1"
+            };
+            JWT_Payload payload = new JWT_Payload
+            {
+                tokenId = tokenId,
+                iss = "localhost",
+                username = userNmae,
+                userId = userId,
+                group = group,
+                startTime = UnixTimeHelper.GetTimeStampInLong(),
+                exp =exp ,
+                random = RandomTool.CreatSafeNum(8)
+            };
+            StringBuilder message = new StringBuilder();
+            message.Append(header.alg);
+            message.Append(payload.tokenId);
+            message.Append(payload.iss);
+            message.Append(payload.username);
+            message.Append(payload.userId);
+            message.Append(payload.group);
+            message.Append(payload.startTime);
+            message.Append(payload.exp);
+            message.Append(payload.random);
+            string password = "";
+            string signature = SHAEncrypt_Helper.Hash1Encrypt(message + password);
+            JWT jWT = new JWT()
+            {
+                Header = header,
+                Payload = payload,
+                Signature = signature
+            };
+            return jWT;
+        }
+
+        public static string ToJSon(string base64)
+        {
+            return null;
+
+        }
+        public static string ToBase64(string base64)
+        {
+            return null;
+        }
+        public bool IsValid()
+        {
+            return false;
+        }
+
+
+
+
+    }
+    public class JWT_Header
+    {
+        public string alg { get; set; }
+
+    }
+    public class JWT_Payload
+    {
+        public long tokenId { get; set; }
+        public string iss { get; set; }
+        public string username { get; set; }
+        public long userId { get; set; }
+        public string group { get; set; }
+        public long startTime { get; set; }
+        public long exp { get; set; }
+        public string random { get; set; }
+    }
+}
--- MoreNote/Controllers/BlogController.cs
+++ MoreNote/Controllers/BlogController.cs
@@ -110,8 +110,16 @@ namespace MoreNote.Controllers
 
             return View();
         }
-
-        [Route("{controller=Blog}/{action=Post}/{blogUserName?}/{noteIdHex?}/")]
+        [Route("{controller=Blog}/{action=Post}/{noteIdHex?}/")]
+        public IActionResult Post1(string noteIdHex)
+        {
+            long noteId = MyConvert.HexToLong(noteIdHex);
+            Note note = NoteService.GetNoteById(noteId);
+            User user = UserService.GetUserByUserId(note.UserId);
+            return Post(user.Username, noteIdHex);
+        }
+        
+        [Route("{controller=Blog}/{action=Post}/{blogUserName}/{noteIdHex?}/")]
         public IActionResult Post(string blogUserName,string noteIdHex)
         {
             User blogUser = ActionInitBlogUser(blogUserName);
