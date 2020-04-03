3c347638af4dd15e8fdf0f60b940e8bcdffa725c
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-04-04 01:24:15 +0800

    注册功能开关

---
+++ MoreNote/Config/ConfigSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoreNote.Config
+{
+    public static class ConfigSetting
+    {
+        /// <summary>
+        /// 是否打开注册功能
+        /// </summary>
+        public static bool openRegister=false;
+        /// <summary>
+        /// 是否打开Demo演示功能
+        /// </summary>
+        public static bool openDemo = false;
+
+    }
+}
