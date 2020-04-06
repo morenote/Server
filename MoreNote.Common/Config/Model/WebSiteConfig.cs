b4ff4fe93c6907d094dc59e1d7bfec55ba6992ac
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-04-07 01:08:34 +0800

    又拍云api

--- MoreNote.Common/Config/Model/WebSiteConfig.cs
+++ MoreNote.Common/Config/Model/WebSiteConfig.cs
@@ -4,12 +4,17 @@ using System.Text;
 
 namespace MoreNote.Common.Config.Model
 {
-    public class PostgreSQLConfig
+    public class WebSiteConfig
     {
         public string connection { get; set; }
         /// <summary>
         /// 随机图片的位置
         /// </summary>
         public string randomImageDir { get; set; }
+        public string upyunSecret { get; set; }
+        public string upyunBucket { get; set; }
+        public string upyunUsername { get; set; }
+        public string upyunPassword { get; set; }
+        
     }
 }
---
+++ 又拍云对象储存测试/UpYunTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UpYunLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MoreNote.Common.Config;
+using MoreNote.Common.Config.Model;
+using System.Collections;
+
+namespace UpYunLibrary.Tests
+{
+    [TestClass()]
+    public class UpYunTests
+    {
+        [TestMethod()]
+        public void CreatTokenTest()
+        {
+            WebSiteConfig postgreSQLConfig = ConfigManager.GetPostgreSQLConfig();
+            UpYun upyun = new UpYun("bucket", "username", "password");
+            upyun.secret = postgreSQLConfig.upyunSecret; ;
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            unixTimestamp += 120;
+            string _upt = upyun.CreatToken(unixTimestamp.ToString(), upyun.secret, "/74956136_p0.jpg");
+            Console.WriteLine($"https://upyun.morenote.top/74956136_p0.jpg?_upt={_upt}");
+        }
+
+        [TestMethod()]
+        public void UpYunTest()
+        {
+            WebSiteConfig postgreSQLConfig = ConfigManager.GetPostgreSQLConfig();
+            UpYun upyun = new UpYun(postgreSQLConfig.upyunBucket, postgreSQLConfig.upyunUsername, postgreSQLConfig.upyunPassword);
+            ArrayList str = upyun.readDir("/");
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
