e976b9adaaff336c141d87882e103887a01b1294
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-04-18 11:34:37 +0800

    配置：是否开启token防盗链

---
+++ UpYunLibrary/ContentRecognition/ContentIdentifiesHookMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace UpYunLibrary.ContentRecognition
+{
+    /// <summary>
+    /// 内容识别操作动作钩子消息
+    /// </summary>
+    public class ContentIdentifiesHookMessages
+    {
+        public string operation { get; set; }
+        public string content { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+        public string task_id { get; set; }
+    }
+}
