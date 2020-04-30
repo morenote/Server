6b35805e81d9d9fb07fc269bcfb32fcbc2c4a41e
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-04-30 22:43:30 +0800

    反黄色图片

--- UpYunLibrary/ContentRecognition/ContentIdentifiesHookMessages.cs
+++ UpYunLibrary/ContentRecognition/ContentIdentifiesHookMessages.cs
@@ -11,14 +11,14 @@ namespace UpYunLibrary.ContentRecognition
         public UpyunOperation operation { get; set; }
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public UpyunContent content { get; set; }
-        [JsonConverter(typeof(JsonStringEnumConverter))]
         public string service { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public UpyunType type { get; set; }
         public string uri { get; set; }
         public string task_id { get; set; }
     }
     public enum UpyunOperation { auto, person }
     public enum UpyunContent { image, live , text }
-    public enum UpyunType { delete, shield, cancel_shield , forbidden, cancel_forbidden }
+    public enum UpyunType { delete, shield, cancel_shield , forbidden, cancel_forbidden, test }
 
 }
