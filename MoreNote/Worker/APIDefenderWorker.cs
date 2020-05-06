1662ebee6b24ec2128e23086a78db2636fb5ffa2
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-05-06 23:46:39 +0800

    扫描恶意访问者

---
+++ MoreNote/Worker/APIDefenderWorker.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MoreNote.Common.Config;
+using MoreNote.Common.Config.Model;
+using MoreNote.Common.Util;
+using MoreNote.Common.Utils;
+using MoreNote.Controllers;
+using MoreNote.Logic.Entity;
+using MoreNote.Logic.Service;
+using UpYunLibrary;
+
+namespace MoreNoteWorkerService
+{
+    //定期扫描网站日志，并将恶意访问者的IP加入黑名单
+    public class APIDefenderWorker : BackgroundService
+    {
+        private readonly ILogger<InitRandomImagesWorker> _logger;
+        /// <summary>
+        /// 网站配置
+        /// </summary>
+        static WebSiteConfig config = ConfigManager.GetPostgreSQLConfig();
+        public APIDefenderWorker()
+        {
+
+        }
+   
+        Random random = new Random();
+
+        public APIDefenderWorker(ILogger<InitRandomImagesWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await Check().ConfigureAwait(false);
+                    int time = DateTime.Now.Hour;
+                    //每过60秒随机抓取一次
+                    //频率太高，站长会顺着网线过来打人
+                    await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken).ConfigureAwait(false);
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogInformation(ex.Message, DateTimeOffset.Now);
+                    await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken).ConfigureAwait(false);
+                }
+            }
+        }
+        private async Task Check()
+        {
+
+        }
+      
+       
+    
+    }
+}
