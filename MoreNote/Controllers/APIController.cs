1d245e60a2c8fde6b6a938d31e4d4d78fddf45ea
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-06-03 20:09:07 +0800

    将验证码服务归到API下面

--- MoreNote/Controllers/APIController.cs
+++ MoreNote/Controllers/APIController.cs
@@ -45,10 +45,7 @@ namespace MoreNote.Controllers
         /// </summary>
         private static int _fuseCount = 0;
         private static readonly object _fuseObj = new object();
-        /// <summary>
-        /// 图片游标
-        /// </summary>
-        private readonly int index = 0;
+
         /// <summary>
         /// 随机数组的大小
         /// </summary>
@@ -241,6 +238,42 @@ namespace MoreNote.Controllers
         public async Task<IActionResult> BrowserDetection()
         {
             return Redirect($"https://www.morenote.top/BrowserDetection.js");
+        }
+        /// <summary>
+        /// 获取图形验证码
+        /// </summary>
+        /// <returns></returns>
+        // [HttpGet("VerifyCode")]
+        public async Task VerifyCode()
+        {
+            Response.ContentType = "image/jpeg";
+
+
+            using (var stream = CheckCode.Create(out var code))
+            {
+                var buffer = stream.ToArray();
+
+                //存session
+                HttpContext.Session.SetString("VerifyCode", code.ToLower());
+                //使用标志，不允许重复使用一个验证码。
+                //这个验证码被消费一次后，要置0。
+                HttpContext.Session.SetInt32("VerifyCodeValid", 1);
+                //验证码生成时间。
+                HttpContext.Session.SetInt32("VerifyCodeTime", UnixTimeHelper.GetTimeStampInInt32());
+
+
+                //string sessionID = Request.Cookies["SessionID"];
+                //RedisManager.SetString(sessionID, code);
+
+                // Response.Cookies.Append("code",code);
+
+                // 将验证码的token放入cookie
+                // Response.Cookies.Append(VERFIY_CODE_TOKEN_COOKIE_NAME, await SecurityServices.GetVerifyCodeToken(code));
+
+                await Response.Body.WriteAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
+            }
+
+
         }
 
 
