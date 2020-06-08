11eb3f8fe24e974cbdf5f8a8009adc92a3cbc5e3
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-06-08 21:08:25 +0800

    想写个众筹网站

---
+++ MoreNote.Logic/Entity/CrowdfundingProject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreNote.Logic.Entity
+{
+   public class CrowdfundingProject
+    {
+        public long ProjectId { get; set; }
+        public int Money { get; set; }
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
---
+++ MoreNote/Controllers/CrowdfundingController1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoreNote.Controllers
+{
+    public class CrowdfundingController1 : Controller
+    {
+        /// <summary>
+        /// 先写个众筹网站
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
