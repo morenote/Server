2243452c11784562fb4291c0c0854752c51ff42b
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-06-22 23:50:33 +0800

    垃圾评论过滤服务

---
+++ MoreNote/Controllers/API/SpamController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoreNote.Controllers.API
+{
+    [Route("api/Auth/[action]")]
+    public class SpamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
