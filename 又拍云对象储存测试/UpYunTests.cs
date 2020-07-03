65903c0ece5427713b862de4433d2f8673ac31ae
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-07-03 20:41:25 +0800

    屏蔽掉依赖

--- 又拍云对象储存测试/UpYunTests.cs
+++ 又拍云对象储存测试/UpYunTests.cs
@@ -15,13 +15,13 @@ namespace UpYunLibrary.Tests
         [TestMethod()]
         public void CreatTokenTest()
         {
-            WebSiteConfig postgreSQLConfig = ConfigManager.GetWebConfig();
-            UpYun upyun = new UpYun("bucket", "username", "password");
-            upyun.secret = postgreSQLConfig.upyunSecret; ;
-            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-            unixTimestamp += 120;
-            string _upt = upyun.CreatToken(unixTimestamp.ToString(), upyun.secret, $"/upload/debbb8740a597ca554d194818af6e560/270a9d65cfb903fb89cb21c78bd6bad106b543ca.jpg");
-            Console.WriteLine($"https://upyun.morenote.top/upload/debbb8740a597ca554d194818af6e560/270a9d65cfb903fb89cb21c78bd6bad106b543ca.jpg?_upt={_upt}");
+            //WebSiteConfig postgreSQLConfig = ConfigManager.GetWebConfig();
+            //UpYun upyun = new UpYun("bucket", "username", "password");
+            //upyun.secret = postgreSQLConfig.upyunSecret; ;
+            //Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            //unixTimestamp += 120;
+            //string _upt = upyun.CreatToken(unixTimestamp.ToString(), upyun.secret, $"/upload/debbb8740a597ca554d194818af6e560/270a9d65cfb903fb89cb21c78bd6bad106b543ca.jpg");
+            //Console.WriteLine($"https://upyun.morenote.top/upload/debbb8740a597ca554d194818af6e560/270a9d65cfb903fb89cb21c78bd6bad106b543ca.jpg?_upt={_upt}");
         }
 
         [TestMethod()]
