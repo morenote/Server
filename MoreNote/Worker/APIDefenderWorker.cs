0d15c37e090da700b869c9d51d29e65e4de39ce9
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-07-05 20:42:55 +0800

    测试部署分支

--- MoreNote/Worker/APIDefenderWorker.cs
+++ MoreNote/Worker/APIDefenderWorker.cs
@@ -1,21 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
 using MoreNote.Common.Config;
 using MoreNote.Common.Config.Model;
-using MoreNote.Common.Util;
-using MoreNote.Common.Utils;
-using MoreNote.Controllers;
-using MoreNote.Logic.Entity;
-using MoreNote.Logic.Service;
-using UpYunLibrary;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MoreNoteWorkerService
 {
@@ -23,16 +14,17 @@ namespace MoreNoteWorkerService
     public class APIDefenderWorker : BackgroundService
     {
         private readonly ILogger<RandomImagesCrawlerWorker> _logger;
+
         /// <summary>
         /// 网站配置
         /// </summary>
-        static WebSiteConfig config = ConfigManager.GetWebConfig();
+        private static WebSiteConfig config = ConfigManager.GetWebConfig();
+
         public APIDefenderWorker()
         {
-
         }
-   
-        Random random = new Random();
+
+        private Random random = new Random();
 
         public APIDefenderWorker(ILogger<RandomImagesCrawlerWorker> logger)
         {
@@ -47,23 +39,21 @@ namespace MoreNoteWorkerService
                 {
                     await Check().ConfigureAwait(false);
                     int time = DateTime.Now.Hour;
+
                     //每过60秒随机抓取一次
                     //频率太高，站长会顺着网线过来打人
                     await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken).ConfigureAwait(false);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     _logger.LogInformation(ex.Message, DateTimeOffset.Now);
                     await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken).ConfigureAwait(false);
                 }
             }
         }
+
         private async Task Check()
         {
-
         }
-      
-       
-    
     }
 }
