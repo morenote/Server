6909a7b046720e5e5b5631e0e11eb276a3ce601d
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-07-05 20:31:09 +0800

    测试actions是否和预期的一样

--- MoreNote/Controllers/PayJSController.cs
+++ MoreNote/Controllers/PayJSController.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
 using MoreNote.Common.Config;
 using MoreNote.Common.Config.Model;
 using MoreNote.Common.Utils;
 using MoreNote.Logic.DB;
 using MoreNote.Logic.Entity;
+
 using PAYJS_CSharp_SDK;
 using PAYJS_CSharp_SDK.Model;
+
+using System;
+using System.Linq;
+using System.Net;
 
 namespace MoreNote.Controllers
 {
     [Authorize(Roles = "Admin,SuperAdmin")]
     public class PayJSController : Controller
     {
-        static WebSiteConfig webSiteConfig = ConfigManager.GetWebConfig();
-        static Payjs pay = new Payjs(webSiteConfig.PayJS_MCHID, webSiteConfig.PayJS_Key);
+        private static WebSiteConfig webSiteConfig = ConfigManager.GetWebConfig();
+
+        private static Payjs pay = new Payjs(webSiteConfig.PayJS_MCHID, webSiteConfig.PayJS_Key);
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult NativePay()
         {
-
             long id = SnowFlake_Net.GenerateSnowFlakeID();
             var nativeRequestMessage = new NativeRequestMessage()
             {
@@ -38,8 +38,7 @@ namespace MoreNote.Controllers
                 attch = "userid",
                 notify_url = "http://frp.morenote.top:7001/PayJS/AsyncNotification"
             };
-         
-           
+
             var responseMessage = pay.native(nativeRequestMessage);
             ViewBag.msg = responseMessage;
             GoodOrder goodOrder = new GoodOrder()
@@ -55,16 +54,16 @@ namespace MoreNote.Controllers
                 payjs_order_id = responseMessage.payjs_order_id,
                 NativeRequestMessage = nativeRequestMessage.ToJsonString(),
                 NativeResponseMessage = responseMessage.ToJsonString()
-
             };
             using (var db = new DataContext())
             {
-                var orderObj = db.GoodOrder.Add(goodOrder);     
+                var orderObj = db.GoodOrder.Add(goodOrder);
                 db.SaveChanges();
             }
-            
+
             return View();
         }
+
         /// <summary>
         /// 支付成功异步通知接口
         /// </summary>
@@ -72,9 +71,8 @@ namespace MoreNote.Controllers
         /// <returns></returns>
         public IActionResult AsyncNotification(NotifyResponseMessage notifyResponseMessage)
         {
-            if (notifyResponseMessage==null)
+            if (notifyResponseMessage == null)
             {
-              
                 Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 return Content("BadRequest");
             }
@@ -84,7 +82,6 @@ namespace MoreNote.Controllers
                 bool identify = pay.notifyCheck(notifyResponseMessage);
                 if (!identify)
                 {
-
                     Response.StatusCode = (int)HttpStatusCode.BadRequest;
                     return Content("BadRequest");
                 }
@@ -92,8 +89,8 @@ namespace MoreNote.Controllers
                 {
                     using (var db = new DataContext())
                     {
-                        var orderObj = db.GoodOrder.Where(b => b.payjs_order_id.Equals(notifyResponseMessage.payjs_order_id)).FirstOrDefault() ;
-                        if (orderObj.total_fee!=notifyResponseMessage.total_fee)
+                        var orderObj = db.GoodOrder.Where(b => b.payjs_order_id.Equals(notifyResponseMessage.payjs_order_id)).FirstOrDefault();
+                        if (orderObj.total_fee != notifyResponseMessage.total_fee)
                         {
                             throw new Exception("金额不正确");
                         }
@@ -105,7 +102,6 @@ namespace MoreNote.Controllers
                         db.SaveChanges();
                         Response.StatusCode = (int)HttpStatusCode.OK;
                         return Content("OK");
-
                     }
                 }
                 catch (Exception)
@@ -113,11 +109,7 @@ namespace MoreNote.Controllers
                     Response.StatusCode = (int)HttpStatusCode.BadRequest;
                     return Content("BadRequest");
                 }
-             
-                
             }
-           
-
         }
     }
 }
