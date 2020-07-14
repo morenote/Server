381a2602b42aac95c0ebf6b1dda3e5d8c4384eec
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-07-14 23:47:09 +0800

    增加垃圾评论识别功能

---
+++ Library/UpYunLibrary/ContentRecognition/ContentIdentifiesHookMessages.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UpYunLibrary.ContentRecognition
+{
+    /// <summary>
+    /// 内容识别操作动作钩子消息
+    /// </summary>
+    public class ContentIdentifiesHookMessages
+    {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UpyunOperation operation { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UpyunContent content { get; set; }
+        public string service { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UpyunType type { get; set; }
+        public string uri { get; set; }
+        public string task_id { get; set; }
+    }
+    public enum UpyunOperation { auto, person }
+    public enum UpyunContent { image, live , text }
+    public enum UpyunType { delete, shield, cancel_shield , forbidden, cancel_forbidden, test }
+
+}
---
+++ Library/UpYunLibrary/OSS/UPYunOSSOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpYunLibrary.OSS
+{
+    public class UPYunOSSOptions
+    {
+        public string bucket { get; set; }
+        public string save_key { get; set; }
+        public int expiration { get; set; }
+
+    }
+}
