90b57c7dec1d5f7657bdf60a58d12fe40f16c776
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-08-21 20:41:29 +0800

    更新日期格式

---
+++ 单元测试/通用类库测试/ExtensionMethods/LeanoteLongExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoreNote.Common.ExtensionMethods;
+using MoreNote.Common.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreNote.Common.ExtensionMethods.Tests
+{
+    [TestClass()]
+    public class LeanoteLongExtensionsTests
+    {
+        [TestMethod()]
+        public void ToLongByHexTest()
+        {
+            long id=SnowFlakeNet.GenerateSnowFlakeID();
+            Console.WriteLine(id);
+            string hex24=id.ToHex24();
+            Console.WriteLine(hex24);
+            long id24=hex24.ToLongByHex();
+            Console.WriteLine(id24);
+        }
+    }
+}
