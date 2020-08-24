940ef61f74c07b488cadadef160b62126fcc8844
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-08-24 20:23:44 +0800

    1

--- 单元测试/NickelProject.LogicTests/Service/NoteServiceTests.cs
+++ 单元测试/NickelProject.LogicTests/Service/NoteServiceTests.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoreNote.Common.Utils;
+using MoreNote.Logic.DB;
 using MoreNote.Logic.Entity;
 using MoreNote.Logic.Service;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace MoreNote.Logic.Service.Tests
 {
@@ -17,6 +20,37 @@ namespace MoreNote.Logic.Service.Tests
         [TestMethod()]
         public void GetNoteContentTest()
         {
+            //using (var db = DataContext.getDataContext())
+            //{
+            //    var list = db.NoteContent.ToList();
+            //    foreach (var item in list)
+            //    {
+            //        if (item.Content==null)
+            //        {
+            //            continue;
+
+            //        }
+            //        Console.WriteLine(item.NoteId);
+            //        //11ae1ade40021000aaaaaaaa
+            //        //处理图片链接
+            //        Regex regexImage = new Regex(@"(https*://[^/]*?/api/file/getImage\?fileId=)([a-zA-Z0-9]{16})aaaaaaaa");
+            //        if (regexImage.IsMatch(item.Content))
+            //        {
+            //            item.Content = regexImage.Replace(item.Content, "${1}" + "00000000"+"${2}");
+            //            Console.WriteLine(item.Content);
+            //        }
+                       
+            //        //处理附件链接
+            //        Regex regexFuJian = new Regex(@"(https*://[^/]*?/api/file/getAttach\?fileId=)([a-zA-Z0-9]{16})aaaaaaaa");
+            //        if (regexFuJian.IsMatch(item.Content))
+            //        {
+            //            item.Content = regexFuJian.Replace(item.Content,  "${1}" + "00000000"+"${2}");
+            //        }
+                        
+            //    }
+            //    db.SaveChanges();
+               
+            //}
     
         }
 
