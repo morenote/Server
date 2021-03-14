06d216c5cd7e1c4934c30dac8e150e06a0b52a01
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-03-14 22:12:52 +0800

    国际化

---
+++ Src/MoreNote.Language/LanguageFactory.cs
@@ -0,0 +1,25 @@
+﻿using MoreNote.Language.Model;
+using MoreNote.Value;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Language
+{
+     public  class LanguageFactory
+    {
+
+        public static LanguageResource GetLanguageResource(string language)
+        {
+
+        }
+        public static  LanguageResource GetLanguageResource(LanguageEnum languageEnum)
+        {
+            string dirName="zh-cn";
+           
+           
+        }
+    }
+}
