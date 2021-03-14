4ae3065ba685f20a69d474b70cc46850fa88e36d
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-03-14 21:45:29 +0800

    改善项目架构

---
+++ Src/MoreNote.Language/Model/LanguageEnum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Language.Model
+{
+    public enum LanguageEnum
+    {
+       /// <summary>
+       /// 德语
+       /// </summary>
+       DE_DE,
+       /// <summary>
+       ///美式英语
+       /// </summary>
+       EN_US,
+       /// <summary>
+       /// 西班牙语
+       /// </summary>
+       ES_CO,
+       /// <summary>
+       /// 法语
+       /// </summary>
+       FR_FR,
+       /// <summary>
+       /// 葡萄牙语
+       /// </summary>
+       PT_PT,
+       /// <summary>
+       /// 简体中文
+       /// </summary>
+       ZH_CN,
+       /// <summary>
+       /// 繁体中文
+       /// </summary>
+       ZH_HK,
+      /// <summary>
+      /// 日语
+      /// </summary>
+       JA,
+       /// <summary>
+       /// 俄语
+       /// </summary>
+       RU
+    }
+}
