b6f0ae606816aca342bf0295457ad48716aadd8a
hyfree <wangxianqiangcn@foxmail.com>
2021-06-21 22:39:37 +0800

    修改数据库

---
+++ Src/MoreNote.Logic/Migrations/20210621143740_TagMap.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoreNote.Logic.Migrations
+{
+    public partial class TagMap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsBlog",
+                table: "note_tag",
+                newName: "is_blog");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_blog",
+                table: "note_tag_map",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_blog",
+                table: "note_tag_map");
+
+            migrationBuilder.RenameColumn(
+                name: "is_blog",
+                table: "note_tag",
+                newName: "IsBlog");
+        }
+    }
+}
