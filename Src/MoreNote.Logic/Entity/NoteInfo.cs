ff99233dbb4c13a36923bcbefe94ff1478e84b9c
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-08-31 20:29:36 +0800

    暂时关闭

--- Src/MoreNote.Logic/Entity/NoteInfo.cs
+++ Src/MoreNote.Logic/Entity/NoteInfo.cs
@@ -8,41 +8,38 @@ using Masuit.LuceneEFCore.SearchEngine;
 namespace MoreNote.Logic.Entity
 {
     [Table("note")]
-    public class Note:LuceneIndexableBaseEntity
+    public class Note
     {
         // // 必须要设置bson:"_id" 不然mgo不会认为是主键
         [Key] 
-        [Column("note_id"),LuceneIndex] 
-        public long? NoteId
-        {
-            get => this.Id;
-            set => this.Id = value;
-        }
-        [Column("user_id"),LuceneIndex]
+        [Column("note_id")] 
+        public long? NoteId{ get; set; }
+        
+        [Column("user_id")]
         public long? UserId { get; set; }//  // 谁的
-        [Column("created_user_id"),LuceneIndex]
+        [Column("created_user_id")]
         public long? CreatedUserId { get; set; }// // 谁创建的(UserId != CreatedUserId, 是因为共享). 只是共享才有, 默认为空, 不存 必须要加omitempty
-        [Column("notebook_id"),LuceneIndex]
+        [Column("notebook_id")]
         public long? NotebookId { get; set; }
-        [Column("title"),LuceneIndex]
+        [Column("title")]
         public string Title { get; set; }//标题
-        [Column("desc"),LuceneIndex(IsHtml = true)]
+        [Column("desc")]
         public string Desc { get; set; }//描述, 非html
-        [Column("src"),LuceneIndex]
+        [Column("src")]
         public string Src { get; set; }//来源, 2016/4/22
         [Column("img_src")]
         public string ImgSrc { get; set; }//图片, 第一张缩略图地址
-        [Column("tags"),LuceneIndex]
+        [Column("tags")]
         public string[] Tags { get; set; }
-        [Column("is_trash"),LuceneIndex]
+        [Column("is_trash")]
         public bool IsTrash { get; set; }//是否是trash, 默认是false
-        [Column("is_blog"),LuceneIndex]
+        [Column("is_blog")]
         public bool IsBlog { get; set; }/// 是否设置成了blog 2013/12/29 新加
-        [Column("url_title"),LuceneIndex]
+        [Column("url_title")]
         public string UrlTitle { get; set; }// // 博客的url标题, 为了更友好的url, 在UserId, UrlName下唯一
         [Column("is_recommend")]
         public bool IsRecommend { get; set; }// 是否为推荐博客 2014/9/24新加
-        [Column("is_top"),LuceneIndex]
+        [Column("is_top")]
         public bool IsTop { get; set; }//log是否置顶
         [Column("has_self_defined")]
         public bool HasSelfDefined { get; set; } // 是否已经自定义博客图片, desc, abstract
@@ -55,29 +52,29 @@ namespace MoreNote.Logic.Entity
         [Column("comment_num")]
         public int CommentNum { get; set; } // 评论次数 
 
-        [Column("is_markdown"),LuceneIndex]
+        [Column("is_markdown")]
         public bool IsMarkdown { get; set; }// 是否是markdown笔记, 默认是false
         [Column("attach_num")]
         public int AttachNum { get; set; }//// 2014/9/21, attachments num
-        [Column("created_time"),LuceneIndex]
+        [Column("created_time")]
         public DateTime CreatedTime { get; set; }
-        [Column("updated_time"),LuceneIndex]
+        [Column("updated_time")]
         public DateTime UpdatedTime { get; set; }
-        [Column("recommend_time"),LuceneIndex]
+        [Column("recommend_time")]
         public DateTime RecommendTime { get; set; }// 推荐时间
-        [Column("public_time"),LuceneIndex]
+        [Column("public_time")]
         public DateTime PublicTime { get; set; } // 发表时间, 公开为博客则设置
-        [Column("updated_user_id"),LuceneIndex]
+        [Column("updated_user_id")]
         public long? UpdatedUserId { get; set; } // 如果共享了, 并可写, 那么可能是其它他修改了
 
         // 2015/1/15, 更新序号
         [Column("usn")]
         public int Usn { get; set; } // UpdateSequenceNum 
-        [Column("is_deleted"),LuceneIndex]
+        [Column("is_deleted")]
         public bool IsDeleted { get; set; } // 删除位 
-        [Column("is_public_share"),LuceneIndex]
+        [Column("is_public_share")]
         public bool IsPublicShare { get; set; }
-        [Column("content_id"),LuceneIndex]
+        [Column("content_id")]
         public long? ContentId { get; set; }//当前笔记的笔记内容 
 
 
@@ -90,37 +87,31 @@ namespace MoreNote.Logic.Entity
     /// </para>
     /// </summary>
     [Table("note_content")]
-    public class NoteContent:LuceneIndexableBaseEntity
+    public class NoteContent
     {
-       
         [Key]
-        [Column("note_content_id"),LuceneIndex]
-
+        [Column("note_content_id")]
         //[JsonConverter(typeof(string))] //解决序列化时被转成数值的问题
-        public long? NoteContentId 
-        {
-            get => this.Id;
-            set => this.Id = value;
-        }
-        [Column("note_id"),LuceneIndex]
+        public long? NoteContentId {get;set;}
+        [Column("note_id")]
         public long? NoteId { get; set; }
-        [Column("user_id"),LuceneIndex]
+        [Column("user_id")]
         public long? UserId { get; set; }
-        [Column("is_blog"),LuceneIndex]
+        [Column("is_blog")]
         public bool IsBlog { get; set; } // 为了搜索博客 
-        [Column("content"),LuceneIndex(IsHtml = true)]
+        [Column("content")]
         public string Content { get; set; }//内容
 
         //public string WebContent{ get;set;}//为web页面优化的内容
-        [Column("abstract"),LuceneIndex]
+        [Column("abstract")]
         public string Abstract { get; set; } // 摘要, 有html标签, 比content短, 在博客展示需要, 不放在notes表中
-        [Column("created_time"),LuceneIndex]
+        [Column("created_time")]
         public DateTime CreatedTime { get; set; }
-        [Column("updated_time"),LuceneIndex]
+        [Column("updated_time")]
         public DateTime UpdatedTime { get; set; }
-        [Column("updated_user_id"),LuceneIndex]
+        [Column("updated_user_id")]
         public long? UpdatedUserId { get; set; } // 如果共享了,  并可写, 那么可能是其它他修改了
-        [Column("is_history"),LuceneIndex]
+        [Column("is_history")]
         public bool IsHistory { get; set; }//是否是历史纪录
     }
     // 基本信息和内容在一起
