03c8b85209c63ef04c6d3fb4bbb41507dca1e856
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-09-11 22:57:58 +0800

    优化ts

--- Src/MoreNote/wwwroot/js/demo/demo.ts
+++
@@ -1,8 +0,0 @@
-﻿class Startup {
-    public static main(): number {
-        console.log('Hello World');
-        return 2;
-    }
-}
-
-Startup.main();
---
+++ Src/MoreNote/wwwroot/typescript/demo/demo.js
@@ -0,0 +1,16 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+        /**
+         *ddd
+         */
+        this.isDone = false;
+    }
+    Startup.main = function () {
+        var startup = new Startup();
+        startup.isDone = false;
+        return 1;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=demo.js.map
