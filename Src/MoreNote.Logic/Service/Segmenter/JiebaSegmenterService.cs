3b98925f9aaf98188dbf9567220228685fe2f389
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-09-13 07:39:16 +0800

    增加新的全文检索查询命令

--- Src/MoreNote.Logic/Service/Segmenter/JiebaSegmenterService.cs
+++ Src/MoreNote.Logic/Service/Segmenter/JiebaSegmenterService.cs
@@ -11,6 +11,11 @@ namespace MoreNote.Logic.Service.Segmenter
 {
     public class JiebaSegmenterService
     {
+        /// <summary>
+        /// 全文检索处理
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
         public NpgsqlTsVector GetNpgsqlTsVector(string content)
         {
             NpgsqlTsVector vector;
@@ -24,7 +29,7 @@ namespace MoreNote.Logic.Service.Segmenter
                     return null; ;
                 }
                 string noHtmlConent = htmlToTextHelper.Convert(content);
-                var list = segmenter.Cut(noHtmlConent, hmm: true);
+                var list = segmenter.CutForSearch(noHtmlConent, hmm: true);
                 var cutList = new List<string>();
                 foreach (var item in list)
                 {
@@ -52,8 +57,26 @@ namespace MoreNote.Logic.Service.Segmenter
             string result = string.Join(" ", list);
             return result;
         }
+        /// <summary>
+        /// 全文检索 查询
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
         public NpgsqlTsQuery GetSerachNpgsqlTsQuery(string keyword)
         {
+
+            if (keyword.Contains('&'))
+            {
+                string[] keys=keyword.Split('&');
+                return GetSerachNpgsqlTsQuery_And(keys);
+            }
+            if (keyword.Contains("|"))
+            {
+                string[] keys=keyword.Split('|');
+                return GetSerachNpgsqlTsQuery_And(keys);
+            }
+
+
             NpgsqlTsQuery vector;
              try
             {
@@ -64,9 +87,61 @@ namespace MoreNote.Logic.Service.Segmenter
                     return null; ;
                 }
                 string noHtmlConent = htmlToTextHelper.Convert(keyword);
-                var list = segmenter.CutForSearch(noHtmlConent, hmm: true);
+                var list = segmenter.Cut(noHtmlConent, hmm: true);
                 var cutList = new List<string>();
                 foreach (var item in list)
+                {
+                    if (item.Length > 1)
+                    {
+                        cutList.Add(item.ToUpper());
+                    }
+                }
+                string str = string.Join(" & ", cutList);
+                vector = NpgsqlTsQuery.Parse(str);
+                
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return vector;
+        }
+
+         public NpgsqlTsQuery GetSerachNpgsqlTsQuery_Or(string[] keywords)
+        {
+            NpgsqlTsQuery vector;
+             try
+            {
+                
+                var cutList = new List<string>();
+                foreach (var item in keywords)
+                {
+                    if (item.Length > 1)
+                    {
+                        cutList.Add(item.ToUpper());
+                    }
+                }
+                string str = string.Join(" | ", cutList);
+                vector = NpgsqlTsQuery.Parse(str);
+                
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return vector;
+        }
+
+        public NpgsqlTsQuery GetSerachNpgsqlTsQuery_And(string[] keywords)
+        {
+            NpgsqlTsQuery vector;
+             try
+            {
+                
+                var cutList = new List<string>();
+                foreach (var item in keywords)
                 {
                     if (item.Length > 1)
                     {
