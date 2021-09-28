eccc1ad015d644d8c562030e4eda9b463775a041
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-09-28 08:30:27 +0800

    获取服务器版本信息

---
+++ Src/MoreNote.Logic/Models/DTO/Joplin/E2EE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Logic.Models.DTO.Joplin
+{
+    public class E2EE
+    {
+        public bool value{get;set;}
+        public long updatedTime{get;set;}
+
+    }
+}
---
+++ Src/MoreNote.Logic/Models/DTO/Joplin/JoplinServerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Logic.Models.DTO.Joplin
+{
+   public class JoplinServerInfo
+    {
+        public int version{get;set;}=3;
+
+        public E2EE e2ee{get;set;}=new E2EE();
+        public ActiveMasterKeyId activeMasterKeyId{get;set;}=new ActiveMasterKeyId();
+        public List<string> masterKeys=new List<string>();
+    }
+}
