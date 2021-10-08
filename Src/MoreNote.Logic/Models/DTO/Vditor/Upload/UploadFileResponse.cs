b7ef5c1146b23e9b651762596be03bfdd8b594b4
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-10-08 23:03:58 +0800

    vditor图片上传功能

---
+++ Src/MoreNote.Logic/Models/DTO/Vditor/Upload/UploadFileResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Logic.Models.DTO.Vditor.Upload
+{
+    public   class UploadFileResponse
+    {
+
+        public string msg{ get;set;}=string.Empty;
+        public int code{ get;set;}=0;
+        public UploadData data{get;set;}
+    }
+}
