7e5e68d7ede08cba52b7cc5e8ada2030f45f3799
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-10-10 09:32:32 +0800

    更新数据库

---
+++ Src/MoreNote.Logic/Migrations/20211009113215_addNoteFileIndex.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoreNote.Logic.Migrations
+{
+    public partial class addNoteFileIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_random_image_random_image_id_type_name_sex_is_delete_block_~",
+                table: "random_image",
+                columns: new[] { "random_image_id", "type_name", "sex", "is_delete", "block", "file_sha1" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_note_file_file_id_user_id_sha1",
+                table: "note_file",
+                columns: new[] { "file_id", "user_id", "sha1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_random_image_random_image_id_type_name_sex_is_delete_block_~",
+                table: "random_image");
+
+            migrationBuilder.DropIndex(
+                name: "IX_note_file_file_id_user_id_sha1",
+                table: "note_file");
+        }
+    }
+}
