c48b4d7f393a45ba9f787742f2fdf06bacb12296
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-10-10 19:27:38 +0800

    更新vditor

--- Src/MoreNote/wwwroot/js/app/vditor.js
+++ Src/MoreNote/wwwroot/js/app/vditor.js
@@ -35,44 +35,7 @@ function initVditor(){
           enable: true,
           type: 'text',
         },
-        hint: {
-          emojiPath: 'https://cdn.jsdelivr.net/npm/vditor@1.8.3/dist/images/emoji',
-          emojiTail: '<a href="https://ld246.com/settings/function" target="_blank">设置常用表情</a>',
-          emoji: {
-            'sd': '💔',
-            'j': 'https://unpkg.com/vditor@1.3.1/dist/images/emoji/j.png',
-          },
-          parse: false,
-          extend: [
-            {
-              key: '@',
-              hint: (key) => {
-                console.log(key)
-                if ('vanessa'.indexOf(key.toLocaleLowerCase()) > -1) {
-                  return [
-                    {
-                      value: '@Vanessa',
-                      html: '<img src="https://avatars0.githubusercontent.com/u/970828?s=60&v=4"/> Vanessa',
-                    }]
-                }
-                return []
-              },
-            },
-            {
-              key: '#',
-              hint: (key) => {
-                console.log(key)
-                if ('vditor'.indexOf(key.toLocaleLowerCase()) > -1) {
-                  return [
-                    {
-                      value: '#Vditor',
-                      html: '<span style="color: #999;">#Vditor</span> ♏ 一款浏览器端的 Markdown 编辑器，支持所见即所得（富文本）、即时渲染（类似 Typora）和分屏预览模式。',
-                    }]
-                }
-                return []
-              },
-            }],
-        },
+       
         
         tab: '\t',
         upload: {
