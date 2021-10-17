076c15ae9b78d6c16b0987df7d810c21748a1c4e
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-10-17 08:42:58 +0800

    增加github.com/ldqk/Masuit.MyBlogs/issues的代码copy

---
+++ Src/MoreNote/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * copy from https://github.com/ldqk/Masuit.MyBlogs
+ * MIT License
+ * Change:init
+ * */
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoreNote.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult FileManager()
+        {
+            return View();
+        }
+    }
+}
