29e48f7a53ee73d2c0db8d1c3d2a6c692975cbfd
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-11-27 22:00:39 +0800

    分离出模型层

--- Src/MoreNote.Models/Entity/Leanote/NoteInfo.cs
+++ Src/MoreNote.Models/Entity/Leanote/NoteInfo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Masuit.LuceneEFCore.SearchEngine;
 using NpgsqlTypes;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
