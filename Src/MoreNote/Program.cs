efd3deecfc017b74b8432d2fa85ee47d4664832d
wangxianqiang <wangxianqaingcn@foxmail.com>
2022-02-22 17:34:51 +0800

    数据库迁移

--- Src/MoreNote/Program.cs
+++ Src/MoreNote/Program.cs
@@ -6,7 +6,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MoreNote.Common.Utils;
 using MoreNote.Logic.Database;
+using MoreNote.Logic.Entity;
 using MoreNote.Logic.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,30 +18,36 @@ namespace MoreNote
     {
         public static void Main(string[] args)
         {
-          
-            //InitSecret();//初始化安全密钥
-           var host=  CreateHostBuilder(args).Build();
+
+           
+            var host = CreateHostBuilder(args).Build();
             var map = GetArgsMap(args);
-            if (map.Keys.Contains("migrate"))
+
+            DeployService deployService = new DeployService(host);
+            if (map.Keys.Contains("command"))
             {
-                //执行数据库迁移命令
-                System.Console.WriteLine("====================PostgreSQL migrate=========================");
-                using (var scope = host.Services.CreateScope())
+                var cmd = map["command"];
+                
+                switch (cmd)
                 {
-                    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
-                    
-                    db.Database.Migrate();
+                    case "GenSecret":
+                        deployService.InitSecret();
+                        return;
+                    case "MigrateDatabase":
+                        deployService.MigrateDatabase();
+                        return;
+                    default:
+                        Console.WriteLine("unkown cmd");
+                        return;
                 }
 
             }
-
-
-
+           
 
             host.Run();
         }
 
-        static Dictionary<string, string> GetArgsMap(string[] args)
+        private static Dictionary<string, string> GetArgsMap(string[] args)
         {
             var map = new Dictionary<string, string>();
             for (int index = 0; index < args.Length; index++)
@@ -59,20 +67,13 @@ namespace MoreNote
                 }
             }
             return map;
-
         }
 
         /// <summary>
         /// 初始化安全秘钥
         /// </summary>
-        private static void InitSecret()
-        {
-            //每次启动程序都会重新初始化Secret
-            ConfigFileService configFileService = new ConfigFileService();
-            configFileService.WebConfig.SecurityConfig.Secret = RandomTool.CreatSafeRandomBase64(32);
-            configFileService.Save();
-            System.Console.WriteLine("InitSecret Success");
-        }
+      
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
             .UseServiceProviderFactory(new AutofacServiceProviderFactory())
@@ -82,7 +83,7 @@ namespace MoreNote
             })
             .ConfigureLogging((context, loggingBuilder) =>
             {
-                 //loggingBuilder.ClearProviders();
+                //loggingBuilder.ClearProviders();
                 loggingBuilder.AddFilter("System", LogLevel.Warning);
                 loggingBuilder.AddFilter("Microsoft", LogLevel.Warning);//过滤掉系统自带的System，Microsoft开头的，级别在Warning以下的日志
                 loggingBuilder.AddLog4Net("config/log4net.config"); //会读取appsettings.json的Logging:LogLevel:Default级别
