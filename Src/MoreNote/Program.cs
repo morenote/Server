32fb5c999417522b4c6b3f9fa94a52e3187c1052
wangxianqiang <wangxianqaingcn@foxmail.com>
2022-02-22 15:57:03 +0800

    数据库迁移命令

--- Src/MoreNote/Program.cs
+++ Src/MoreNote/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MoreNote.Common.Utils;
+using MoreNote.Logic.Database;
 using MoreNote.Logic.Service;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MoreNote
@@ -12,9 +16,52 @@ namespace MoreNote
     {
         public static void Main(string[] args)
         {
+          
             //InitSecret();//初始化安全密钥
-            CreateHostBuilder(args).Build().Run();
+           var host=  CreateHostBuilder(args).Build();
+            var map = GetArgsMap(args);
+            if (map.Keys.Contains("migrate"))
+            {
+                //执行数据库迁移命令
+                System.Console.WriteLine("====================PostgreSQL migrate=========================");
+                using (var scope = host.Services.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+                    
+                    db.Database.Migrate();
+                }
+
+            }
+
+
+
+
+            host.Run();
         }
+
+        static Dictionary<string, string> GetArgsMap(string[] args)
+        {
+            var map = new Dictionary<string, string>();
+            for (int index = 0; index < args.Length; index++)
+            {
+                string arg = args[index];
+                if (arg.StartsWith("-"))
+                {
+                    if ((index + 1) < args.Length && (!args[index + 1].StartsWith("-")))
+                    {
+                        map.Add(arg.Replace("-", ""), args[index + 1]);
+                        index++;
+                    }
+                    else
+                    {
+                        map.Add(arg.Replace("-", ""), null);
+                    }
+                }
+            }
+            return map;
+
+        }
+
         /// <summary>
         /// 初始化安全秘钥
         /// </summary>
