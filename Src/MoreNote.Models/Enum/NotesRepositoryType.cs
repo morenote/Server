a52e38bfed9b7e09f3a497722fe8f7d68ee9499d
hyfree-msi <wangxianqiangcn@foxmail.com>
2022-02-23 23:29:59 +0800

    增加笔记仓库的设定

---
+++ Src/MoreNote.Models/Enum/NotesRepositoryType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Models.Enum
+{
+    /// <summary>
+    /// 笔记仓库类型
+    /// </summary>
+    public enum NotesRepositoryType
+    {
+        //个人仓库
+        Personal=0x01,
+        //组织仓库
+        Organization=0x02
+
+    }
+}
