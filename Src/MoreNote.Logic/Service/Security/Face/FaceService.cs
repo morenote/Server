ab98040cdfb597b7cb3cced29b09ba3435d2103b
hyfree <wangxianqiangcn@foxmail.com>
2022-02-25 18:16:24 +0800

    FIDO2

---
+++ Src/MoreNote.Logic/Service/Security/Face/FaceService.cs
@@ -0,0 +1,56 @@
+﻿using FaceRecognitionDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Logic.Service.Security.Face
+{
+    public class FaceService
+    {
+        /// <summary>
+        /// 人脸模型的文件夹路径
+        /// </summary>
+        string directory = "";
+        /// <summary>
+        /// 容差阈值。如果比较面部的差距小于容差阈值则相同,否则不一样
+        /// </summary>
+        double tolerance = 0.2d;
+        public FaceService(ConfigFileService configFileService)
+        {
+            var config = configFileService.WebConfig.SecurityConfig.FaceConfig;
+            this.directory = config.ModelFilesDirectory;
+            this.tolerance = config.Tolerance;
+        }
+
+
+        public bool CompareFace()
+        {
+           
+            using (FaceRecognition fr = FaceRecognition.Create(directory))
+            {
+
+                using (Image imageA = FaceRecognition.LoadImageFile(@"E:\Share\WangXianQiang\Work\09Face\01.jpg"))
+                using (Image imageB = FaceRecognition.LoadImageFile(@"E:\Share\WangXianQiang\Work\09Face\03.jpg"))
+                {
+
+                    IEnumerable<Location> locationsA = fr.FaceLocations(imageA);
+                    IEnumerable<Location> locationsB = fr.FaceLocations(imageB);
+
+
+                    IEnumerable<FaceEncoding> encodingA = fr.FaceEncodings(imageA, locationsA);
+                    IEnumerable<FaceEncoding> encodingB = fr.FaceEncodings(imageB, locationsB);
+
+                    
+                    bool match = FaceRecognition.CompareFace(encodingA.First(), encodingB.First(), tolerance);
+
+                    return match;
+                }
+
+
+            }
+        }
+
+    }
+}
