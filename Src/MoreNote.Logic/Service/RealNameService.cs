7d70f63491f064f9297e890d3e9a8bafcfbb72fa
hyfree-msi <wangxianqiangcn@foxmail.com>
2022-06-19 20:50:34 +0800

    实名认证的hmac

---
+++ Src/MoreNote.Logic/Service/RealNameService.cs
@@ -0,0 +1,45 @@
+﻿using MoreNote.Logic.Database;
+using MoreNote.Models.Entity.Leanote;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Logic.Service
+{
+    public class RealNameService
+    {
+        DataContext dataContext;
+
+        public RealNameService(DataContext dataContext)
+        {
+            this.dataContext=dataContext;
+        }
+
+        public void SetRealName(long? userId,string realName)
+        {
+
+
+        }
+
+        public void GetRealName(long? userId)
+        {
+
+        }
+        public void verifyHmac(RealNameInformation realName )
+        {
+
+
+        } 
+        private  RealNameInformation DoHamc(RealNameInformation realNameInformation)
+        {
+
+
+
+            return realNameInformation;
+
+        }
+    }
+}
---
+++ Src/MoreNote.Models/Entity/Leanote/RealNameInformation.cs
@@ -0,0 +1,56 @@
+﻿using MoreNote.CryptographyProvider;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Models.Entity.Leanote
+{
+    [Table("report_info")]
+    public class RealNameInformation
+    {
+        [Key]
+        [Column("id")]
+        public long? Id { get;set;}
+        [Column("user_id")]
+        public long? UserId { get;set;}
+        [Column("real_name")]
+        public string? RealName { get;set;}
+        [Column("hmac")]
+        public string? Hmac { get;set;}
+        [NotMapped]
+        public bool Verify { get; set; }
+        public string ToStringNoMac()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Id=" + this.Id);
+            stringBuilder.Append("UserId=" + this.UserId);
+            stringBuilder.Append("RealName=" + this.RealName);
+            return stringBuilder.ToString();
+        }
+
+        public async Task<RealNameInformation> AddMac(ICryptographyProvider cryptographyProvider)
+        {
+            var bytes = Encoding.UTF8.GetBytes(this.ToStringNoMac());
+            var base64 = Convert.ToBase64String(bytes);
+            this.Hmac = await cryptographyProvider.hmac(base64);
+            return this;
+        }
+
+        public async Task<bool> VerifyHmac(ICryptographyProvider cryptographyProvider)
+        {
+            if (string.IsNullOrEmpty(this.Hmac))
+            {
+                return false;
+            }
+            var bytes = Encoding.UTF8.GetBytes(this.ToStringNoMac());
+            var base64 = Convert.ToBase64String(bytes);
+            return await cryptographyProvider.verifyHmac(base64, this.Hmac);
+        }
+
+    }
+}
