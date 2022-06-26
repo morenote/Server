e0ed78fb81c8078a911fc272e84b48a40e233fad
hyfree-msi <wangxianqiangcn@foxmail.com>
2022-06-26 09:52:38 +0800

    支持双因素登录

---
+++ Src/MoreNote/bin/Release/net6.0/publish/docker_buildx.bat
@@ -0,0 +1,6 @@
+docker buildx create --name lilqbuilder
+docker buildx use lilqbuilder
+docker buildx ls
+docker buildx inspect lilqbuilder --bootstrap
+docker buildx ls
+docker buildx build --platform linux/amd64,linux/arm64  -t registry.cn-hangzhou.aliyuncs.com/hyfree/morenote:0.0.3  --push .
