7759902c0522a50dd0acd2007071fc70a06922ff
hyfree-msi <wangxianqiangcn@foxmail.com>
2022-07-21 23:11:39 +0800

    增加存储卷设计

---
+++ Src/MoreNote.StorageVolume/ObjectStorage/MINIO/MinioVolume.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.StorageVolume.ObjectStorage.MINIO
+{
+    public class MinioVolume:StorageVolumeInterface
+    {
+
+    }
+}
---
+++ Src/MoreNote.StorageVolume/StorageVolumeInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.StorageVolume
+{
+    /// <summary>
+    /// 存储卷接口
+    /// </summary>
+    public interface StorageVolumeInterface
+    {
+
+    }
+}
