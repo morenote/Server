d44d0f9a152a48d1865e5ef88d9b44e29a47a158
hyfree-msi <wangxianqiangcn@foxmail.com>
2023-02-12 22:42:45 +0800

    博客处理管道

---
+++ Src/MoreNote.Logic/Service/BlogBuilder/BlogBuilderIPipeInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Logic.Service.BlogBuilder
+{
+    /// <summary>
+    /// 博客生成器管道处理器接口
+    /// </summary>
+    public interface  BlogBuilderPipeInterface
+    {
+        public  string BlogBuilderDataProcessing(string Content);
+      
+    }
+}
---
+++ Src/MoreNote.Logic/Service/BlogBuilder/BlogBuilderPipeExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.Logic.Service.BlogBuilder
+{
+    public static class BlogBuilderPipeExtension
+    {
+        /// <summary>
+        /// 调用管道代码处理数据，并吐出数据
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="blogBuilderPipe"></param>
+        /// <returns></returns>
+        public static string BlogBuilderPipe(this string data, BlogBuilderPipeInterface blogBuilderPipe)
+        {
+            return blogBuilderPipe.BlogBuilderDataProcessing(data);
+        }
+    }
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
