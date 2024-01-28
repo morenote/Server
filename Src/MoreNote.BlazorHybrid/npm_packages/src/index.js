a4374acfef2c7f818da08d24744640372139a1a3
hyfree-msi <wangxianqiangcn@foxmail.com>
2024-01-28 21:41:14 +0800

    控制台程序

---
+++ Src/MoreNote.BlazorHybrid/npm_packages/src/index.js
@@ -0,0 +1,8 @@
+﻿
+import { getCurrentTime } from './time_lib';
+import { vditor } from 'vditor';
+
+export function GetCurrentTime() {
+    
+}
+
---
+++ Src/MoreNote.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoreNote.ConsoleApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
