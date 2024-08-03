94d423db57803a297d9a04e66ba6481287026c2a
hyfree <wangxianqiangcn@foxmail.com>
2024-08-03 13:12:10 +0800

    增加客户端界面

---
+++ Src/Client/MoreNote.BlazorHybridApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoreNote.BlazorHybridApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
