4000135e37d2947e8520d9bd70e3cee206b435bb
hyfree <wangxianqiangcn@foxmail.com>
2024-09-08 18:38:57 +0800

    加载状态

---
+++ Src/Client/MoreNote.BlazorHybridApp/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreNote.BlazorHybridApp.Components.Layout
+{
+    public partial class MainLayout
+    {
+        bool loading = true;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Task.Run(async () => {
+                await Task.Delay(1000);
+                loading = false;
+                await this.InvokeAsync(() => {
+
+                    StateHasChanged();
+                });
+              
+            });
+        }
+    }
+}
